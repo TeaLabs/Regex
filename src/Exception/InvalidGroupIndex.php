ff93bf5ecc2e88b93658e144022d87d5b499aad8
David Kyalo <davidmkyalo@gmail.com>
2017-01-07 12:24:23 +0300

    [UPDATE] Allows an optional 'message' argument.

--- src/Exception/InvalidGroupIndex.php
+++ src/Exception/InvalidGroupIndex.php
@@ -5,13 +5,12 @@ use Exception;
 
 class InvalidGroupIndex extends RegexError
 {
-	public static function create($pattern, $subject, $index)
+	public static function create($pattern, $subject, $index, $message = "String, Integer or Iterable expected.")
 	{
 		$pattern = static::formatObject($pattern);
 		$subject = static::formatObject($subject);
 		$index = static::formatObject($index);
-		return new static("Invalid group index {$index} for matched pattern `{$pattern}` with subject `{$subject}`. ".
-			"String, Integer or Iterable expected.");
+		return new static("Invalid group index {$index} for matched pattern `{$pattern}` with subject `{$subject}`. {$message}");
 	}
 
 }
