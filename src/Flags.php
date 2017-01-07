3cc1b4846809629dc30d6244fc8ec4aa67e51de0
David Kyalo <davidmkyalo@gmail.com>
2017-01-07 14:00:49 +0300

    [ADD] A wrrapper for PREG_* flags..

---
+++ src/Flags.php
@@ -0,0 +1,40 @@
+<?php
+namespace Tea\Regex;
+
+
+/**
+* A wrapper for PREG_* flags.
+*/
+class Flags
+{
+	/**
+	 * @var int
+	 */
+	const OFFSET_CAPTURE = PREG_OFFSET_CAPTURE;
+
+	/**
+	 * @var int
+	 */
+	const PATTERN_ORDER = PREG_PATTERN_ORDER;
+
+	/**
+	 * @var int
+	 */
+	const SET_ORDER = PREG_SET_ORDER;
+
+	/**
+	 * @var int
+	 */
+	const SPLIT_NO_EMPTY = PREG_SPLIT_NO_EMPTY;
+
+	/**
+	 * @var int
+	 */
+	const SPLIT_DELIM_CAPTURE = PREG_SPLIT_DELIM_CAPTURE;
+
+	/**
+	 * @var int
+	 */
+	const SPLIT_OFFSET_CAPTURE; = PREG_SPLIT_OFFSET_CAPTURE;
+
+}
