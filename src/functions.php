3f2b72c596154aa6ea8723fe451152a5f17da403
David Kyalo <davidmkyalo@gmail.com>
2017-01-08 23:37:36 +0300

    [ADD] The re_from() function.

--- src/functions.php
+++ src/functions.php
@@ -19,3 +19,27 @@ function re($body, $modifiers = null, $delimiter = null)
 {
 	return Regex::create($body, $modifiers, $delimiter);
 }
+
+/**
+ * Create a RegularExpression instance from a possibly complete regex string
+ * or a {@see \Tea\Regex\Contracts\Pattern} instance.
+ * If the given pattern is string it will be parsed to extract the regex body,
+ * modifiers and the delimiter if any.
+ *
+ * If either the modifiers and/or delimiter are neither set on the pattern
+ * nor passed as arguments, the defaults {@see \Tea\Regex\Config} will be used.
+ * Modifiers and/or the delimiter passed as arguments will be used instead
+ * of those set on the pattern.
+ *
+ * @see  \Tea\Regex\RegularExpression::from()
+ *
+ * @param  string|\Tea\Regex\Contracts\Pattern  $pattern
+ * @param  string|null|false                    $modifiers
+ * @param  string|null                          $delimiter
+ *
+ * @return \Tea\Regex\RegularExpression
+ */
+function re_from($pattern, $modifiers = null, $delimiter = null)
+{
+	return Regex::from($pattern, $modifiers, $delimiter);
+}
